849edebeb96eaf717b6e0c8f015d27565b39e6e3
Sergiy Zherebchuk <serg.hardy.13@gmail.com>
2016-11-02 16:26:41 +0200

    stage 4-3

---
+++ drupal7/sites/all/modules/my_theming/template.tpl.php
@@ -0,0 +1,10 @@
+<div class="custom-template-wrapper">
+    <div class="body">
+        <h2>
+            <?= $variables['body-head'] ?>
+        </h2>
+        <strong>
+            <?= $variables['body-foot'] ?>
+        </strong>
+    </div>
+</div>
